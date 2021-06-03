83cc5499ee9e5ce859ccac2e0e241ddd85d1fc6f
yusuke-ten <net.escape.com@gmail.com>
2021-06-03 23:52:45 +0900

    format by prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
-  resources :users
-  root 'users#index'
-end
+Rails
+  .application
+  .routes
+  .draw do
+    resources :users
+    root 'users#index'
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_06_03_144153) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
