c46b295346b9a405d0977bcf39b84bc0fd0db91a
yusuke-ten <net.escape.com@gmail.com>
2021-06-04 00:02:00 +0900

    generate microposts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-Rails
-  .application
-  .routes
-  .draw do
-    resources :users
-    root 'users#index'
-  end
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'users#index'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_144153) do
+ActiveRecord::Schema.define(version: 2021_06_03_145855) do
+  create_table 'microposts', force: :cascade do |t|
+    t.text 'content'
+    t.integer 'user_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'email'
