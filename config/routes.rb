eb85351b5c25d346cb851cd7f1afcfd15f90cd83
yusuke-ten <net.escape.com@gmail.com>
2021-06-04 00:02:14 +0900

    format ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
-Rails.application.routes.draw do
-  resources :microposts
-  resources :users
-  root 'users#index'
-end
+Rails
+  .application
+  .routes
+  .draw do
+    resources :microposts
+    resources :users
+    root 'users#index'
+  end
